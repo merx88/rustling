bd1541c72195028582b1046c33badda89911726d
MERx88 <wjang07@gmail.com>
2025-02-08 23:44:11 +0900

    Done Quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -17,6 +17,24 @@
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a vector of strings.
 
+// 다음 섹션에 대한 퀴즈입니다:
+// - 문자열
+// - 벡터
+// - 이동 시맨틱
+// - 모듈
+// - 열거형
+//
+// 함수 형태로 작은 기계를 만들어 봅시다. 입력으로 문자열과 명령어 목록을 줄 것입니다.
+// 이 명령어들은 문자열에 어떤 작업을 적용할지를 결정합니다. 작업은 다음과 같습니다:
+// - 문자열을 대문자로 변환
+// - 문자열을 잘라내기
+// - 지정된 횟수만큼 문자열에 "bar"를 추가
+//
+// 정확한 형태는 다음과 같습니다:
+// - 입력은 2개의 요소로 이루어진 튜플의 벡터가 될 것입니다.
+// 첫 번째 요소는 문자열이고, 두 번째 요소는 명령어입니다.
+// - 출력 요소는 문자열의 벡터가 될 것입니다."
+
 enum Command {
     Uppercase,
     Trim,
@@ -24,10 +42,29 @@ enum Command {
 }
 
 mod my_module {
+    // use std::process::Command;
+
     use super::Command;
 
+    fn append(mut string: String, times: usize) -> String {
+        for _ in 0..times {
+            string.push_str("bar");
+        }
+        string
+    }
+
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: &[(String, Command)]) -> Vec<String> {
+        let mut res = Vec::new();
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => res.push(string.to_uppercase()),
+                Command::Trim => res.push(string.trim().to_string()),
+                Command::Append(times) => res.push(append(string.clone(), *times)),
+            }
+        }
+        res
+    }
 }
 
 fn main() {
@@ -38,6 +75,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -48,7 +86,7 @@ mod tests {
             ("foo".to_string(), Command::Append(1)),
             ("bar".to_string(), Command::Append(5)),
         ];
-        let output = transformer(input);
+        let output = transformer(&input);
 
         assert_eq!(
             output,
